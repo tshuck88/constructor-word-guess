a491d54d6561c296a968203a58e447b657e78939
Tyler Shuck <tshuck88@gmail.com>
2019-11-05 16:19:06 -0800

    changed name of current work array

--- javascript/word.js
+++ javascript/word.js
@@ -3,15 +3,15 @@ const Letter = require("./letter.js");
 const Word = function (word) {
     this.word = word;
     this.letters = [];
-    this.currentWord = [];
+    this.currentWordArray = [];
     for (let i = 0; i < this.word.length; i++) {
         this.letters.push(new Letter(this.word[i]));
     }
     this.displayWord = function () {
         for (let j = 0; j < this.letters.length; j++) {
-            this.currentWord.push(this.letters[j].displayLetter());
+            this.currentWordArray.push(this.letters[j].displayLetter());
         }
-        return this.currentWord.join(" ");
+        return this.currentWordArray.join(" ");
     }
     this.userGuess = function (char) {
         for (let k = 0; k < this.letters.length; k++) {
